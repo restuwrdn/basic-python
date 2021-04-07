83f64752d5beebd03f39193ab7df82e7da33bf2f
restuwrdn <restuwrdn@gmail.com>
2021-04-07 08:41:16 +0800

    Final Project - Basic Python

--- Final_Project/final_project.py
+++ Final_Project/final_project.py
@@ -25,7 +25,7 @@ msg ['Subject'] = subject
 msg ['To'] = ', '.join(recipient)    
 msg.attach(MIMEText(message, "plain"))
 
-file_path = "C:\\...\\<attachment file name>.txt"
+file_path = "C:\\...\\<attachment_file_name>.txt"
 filename = os.path.basename(file_path)
 attachment = open(file_path, "rb")
 
